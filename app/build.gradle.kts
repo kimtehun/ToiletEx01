6542e66aecbb3709905b9f225b8293a192ac0629
kimtehun <rpffhrm@gmail.com>
2025-02-24 18:55:56 +0900

    build.gradle target, compile 버전 35 변경 assets 폴더 추가 assets 폴더 안에 toiletdb.db 파일 추가 toiletdb.db 파일은 db browser 에서 csv 파일에서 전국 화장실정보 인서트 된 파일

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.toiletex01"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.toiletex01"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
