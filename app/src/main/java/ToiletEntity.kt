8729ca30de3bd729b65002bd47f0e3477c62f45c
kimtehun <rpffhrm@gmail.com>
2025-03-01 21:59:12 +0900

    ToiletEntity 라는 데이터 클래스 생성

---
+++ app/src/main/java/ToiletEntity.kt
@@ -0,0 +1,13 @@
+data class ToiletEntity(
+    val num : Int ,
+    val division : String ,
+    val toiletName : String ,
+    val roadNameAddress : String ,
+    val streetNumberAddress : String ,
+    val openingHoursDetails : String ,
+    val installationDate : String ,
+    val latitude : String ,
+    val longitude : String ,
+    val dataBaseDate : String ,
+    val pw : String
+)
